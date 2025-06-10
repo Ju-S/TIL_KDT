963eac82810954adae21e40094eeae53358a16ce
Ju-S <wntjd0643@gmail.com>
2025-06-10 15:33:41 +0900

    20250610_중간저장

---
+++ Day01_Basic/src/Exam_01.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+public class Exam_01 {
+    public static void main(String[] ar) {
+        System.out.println("Hello world");
+        System.out.println(10);
+        System.out.println('A');
+        System.out.println(3.14);
+    }
+}
---
+++ Day01_Basic2/src/Quiz01.java
@@ -0,0 +1,11 @@
+
+public class Quiz01 {
+    public static void main(String[] ar) {
+        System.out.println("===================");
+        System.out.println("      /)/)");
+        System.out.println("     (  ..)");
+        System.out.println("     (  >❤");
+        System.out.println(" Have a Good Time.");
+        System.out.println("===================");
+    }
+}
