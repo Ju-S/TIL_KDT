0b8c6f2a9934ee3f66faf27e55b499bcafd02c81
Ju-S <wntjd0643@gmail.com>
2025-06-13 17:10:20 +0900

    20250613_마무리 trivista_개인 프로젝트 계획

--- KDT_class/Day04_Controls/src/Exam04.java
+++ KDT_class/Day04_Controls/src/Exam04.java
@@ -16,7 +16,7 @@ public class Exam04 {
 
         while (true) {
             System.out.println("<< 자판기 시뮬레이터 >>");
-            System.out.println("1. 콜라(1000) / 2. 사이다(800) / 3. 매실차(1500) / 0. 소지품 / 9. 종료");
+            System.out.println("1. 콜라(" + colaPrice + ") / 2. 사이다(" + ciderPrice + ") / 3. 매실차(" + maesilPrice + ") / 0. 소지품 / 9. 종료");
             System.out.print(">> ");
             int selectedMenu = Integer.parseInt(sc.nextLine());
             // 메뉴 번호 사용자 입력
