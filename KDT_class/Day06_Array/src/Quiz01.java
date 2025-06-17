ffc0e101e04795dc53657bc3c8d7a8a61964ed69
Ju-S <wntjd0643@gmail.com>
2025-06-17 17:10:27 +0900

    20250617_마무리

--- KDT_class/Day06_Array/src/Quiz01.java
+++ KDT_class/Day06_Array/src/Quiz01.java
@@ -1,12 +1,29 @@
 public class Quiz01 {
     public static void main(String[] args) {
+//        int[] arr = new int[100];
+//
+//        for(int i = 0; i < arr.length; i++){
+//            arr[i] = i;
+//        }
+//
+//        for(int i = 0; i < arr.length; i ++) {
+//            System.out.println(arr[i]);
+//        }
+//
+//        System.out.println(arr[0]);
+//        System.out.println(arr[99]);
+
         int[] arr = new int[100];
 
-        for(int i = 0; i < arr.length; i++){
-            arr[i] = i;
+        for(int i = arr.length; i > 0; i--){
+            arr[arr.length - i] = i;
         }
 
-        for(int i = 0; i < arr.length; i ++) {
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = arr.length - i;
+        }
+
+        for(int i = 0; i < arr.length; i++){
             System.out.println(arr[i]);
         }
 
